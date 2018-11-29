16d6cf60a811f8bebc7dd4b2fa2a03a957d45257
Dongbo Shi <stonebird0203@gmail.com>
2018-11-29 16:03:34 +0800

    makefile version of coauthor glue

--- coauthor_glue.R
+++ coauthor_glue.R
@@ -1,16 +1,27 @@
 #coauthor glue 
 
+#!/usr/bin/env Rscript
+require(argparse)
+psr <- ArgumentParser(description="coauthor glue baseline")
+psr$add_argument("ipt", help="input", nargs="+")
+psr$add_argument("-o", dest="opt", help="output")
+
+args <- psr$parse_args()
+
+
 library(igraph)
 library(dplyr)
 library(rjson)
 library(stringr)
+require(plyr)
 
-path <- '/Users/ZLab/Documents/Dropbox/test'
-setwd(paste0(path,'/author0'))
-pv <- list.files()
+#path <- '/Users/ZLab/Documents/Dropbox/test'
+#setwd(paste0(path,'/author0'))
+#pv <- list.files()
 
-for(i in 1: length(pv)){
-        
+pv <- list.files(path = args$ipt)
+
+coauthor_glue <- function(i){
         author<-read.csv(pv[i]) %>% select(-X)
         names(author) <- c('name','org','id','seq')
         fname <- str_replace_all(pv[i],pattern='.csv',replacement = '')
@@ -26,7 +37,7 @@ for(i in 1: length(pv)){
                                     org = str_replace_all(org,pattern = "[:blank:]{2,}",replacement = ' '),
                                     org = str_replace_all(org,pattern = "^[:blank:]+",replacement = ''))
         
-        auname <- author %>% group_by(name) %>% summarise(count = n()) %>% arrange(desc(count))
+        auname <- author %>% group_by(name) %>% dplyr::summarise(count = n()) %>% arrange(desc(count))
         auname <- auname$name[1]        
         # 生成一个邻接矩阵
         node <- unique(author$id)
@@ -44,7 +55,7 @@ for(i in 1: length(pv)){
                 name_1 <- name_1[name_1!=auname]
                 adjacency$merge[adjacency$node1 == id_1&adjacency$node2 %in%
                                         author$id[author$name %in% name_1]] <-1
-                cat(j,'\n')
+                #cat(j,'\n')
         }
         adjacency_1 <- adjacency[adjacency$merge==1,]
         e <- data.frame(from = adjacency_1$node1,
@@ -53,9 +64,14 @@ for(i in 1: length(pv)){
         net <- graph_from_data_frame(e, directed=F, vertices=node)
         
         net_c <- components(net)$membership
-        write_json(net_c,path = paste0(path,'/result/',fname,'.json'))
-        cat(i,'------------------------------------------------------','\n')
+        write_json(net_c,path = paste0(args$opt,'/result/',fname,'.json'))
+        cat(i,'th caculation finished','\n')
 }
+
+l_ply(1:length(pv), coauthor_glue, .parallel=TRUE)
+
+
+
 
 
 
