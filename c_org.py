6904d3b5f45873f6d2ce0ebf5dea3964ba089038
Zhuozhuo <zhuojingzhang@163.com>
2018-11-30 18:09:15 +0800

    c_org working in progress.

--- c_org.py
+++ c_org.py
@@ -4,13 +4,16 @@ psr = argparse.ArgumentParser("baseline solution")
 psr.add_argument("-o", dest='opt', help="output")
 psr.add_argument('ipt', help="input")
 psr.add_argument('--field', default='org', help="the field to count common entries in")
+psr.add_argument('--idf', default='data/train/venue_idf.csv', help="idf feature generator")
 args = psr.parse_args()
 
 import pandas as pd, itertools as it, h5py, numpy as np
+
 # ^^^ command line specification
 
 from collections import Counter
 au = pd.read_csv(args.ipt)
+IDFinput = pd.read_csv(args.idf)
 
 # the central fucntion is sum((Counter(al[1]) & Counter(bl[1])).values())
 
@@ -19,14 +22,22 @@ au = pd.read_csv(args.ipt)
 # counted as 2.
 
 # this is expanded to be used with keywords as well
+def f(al,bl):
+    commondict = Counter(al[1]) & Counter(bl[1])
+    overlap = sum(commondict.values())
+    sumlength = len(al[1]) + len(bl[1])
+    IDF = IDFinput.ix[commondict.index()]*commondict.values()
+    return overlap, sumlength, IDF
 
-dl = ((sum((Counter(al[1]) & Counter(bl[1])).values()), len(al[1])+len(bl[1]))
+
+dl = (f(al,bl)
       for (al, bl) in it.combinations(au.groupby('id')[args.field],2))
 x = np.array(list(dl), dtype='u2')
 
 df = pd.DataFrame({'{}_overlap'.format(args.field): x[:,0], 
                    '{}_share_dummy'.format(args.field): x[:,0]!=0,
-                   '{}_jaccard_similarity_metric'.format(args.field): x[:,0]/(x[:,1]-x[:,0])}
+                   '{}_jaccard_similarity_metric'.format(args.field): x[:,0].astype('float32')/(x[:,1]-x[:,0]),
+                   '{}_logIDF'.format(args.field): x[:,2]}
 )
 
 # output .h5:
